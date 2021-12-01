890a5658d841d1ee650603d93e74142c6d5ff3a0
fray <mars6358@gmail.com>
2021-12-02 01:13:10 +0600

    add user db model

---
+++ backend/app/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base_class import Base
+
+
+class User(Base):
+    __tablename__ = 'user_account'
+
+    id = Column(Integer, primary_key=True)
+    nick = Column(String(30))
+    email = Column(String(40))
+    hashed_password = Column(String(100))
