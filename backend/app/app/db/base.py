b7071064dbe6a8abd7611eac9d4a4ee139b18b84
fray <mars6358@gmail.com>
2021-12-02 01:12:30 +0600

    setup alembic

---
+++ backend/app/app/db/base.py
@@ -0,0 +1,6 @@
+"""
+file used in alembic env.py for 'autogenerate' support
+"""
+
+from app.db.base_class import Base
+from app.models.user import User
--- backend/app/app/db/db.py
+++ backend/app/app/db/db.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from config import DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE
 
 engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}')
 
 Session = sessionmaker(engine)
-Base = declarative_base()
+
